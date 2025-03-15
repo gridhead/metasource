8409ce7ec659aec2cd42447cbacfbff1a8938ca1
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2025-03-15 06:19:38 +0000

    Add a barebones HTTP server for endpoints
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- main.go
+++ main.go
@@ -2,35 +2,48 @@ package main
 
 import (
 	"fmt"
-	"metasource/metasource/models/sxml"
-	"metasource/metasource/runner"
-	"sync"
+	"log/slog"
+	"metasource/metasource/routes"
+	"net/http"
 )
 
 func main() {
-	var name string
-	var rslt_primary *sxml.UnitPrimary
-	var rslt_other *sxml.UnitOther
-	var rslt_filelist *sxml.UnitFileList
-	var wait sync.WaitGroup
+	//var name string
+	//var rslt_primary *sxml.UnitPrimary
+	//var rslt_other *sxml.UnitOther
+	//var rslt_filelist *sxml.UnitFileList
+	//var wait sync.WaitGroup
+	//
+	//name = "obserware"
+	//
+	//wait.Add(3)
+	//go runner.ImportPrimary(&wait, &rslt_primary, &name)
+	//go runner.ImportOther(&wait, &rslt_other, &name)
+	//go runner.ImportFileList(&wait, &rslt_filelist, &name)
+	//wait.Wait()
+	//
+	//if rslt_primary != nil {
+	//	fmt.Println(rslt_primary)
+	//}
+	//
+	//if rslt_other != nil {
+	//	fmt.Println(rslt_other)
+	//}
+	//
+	//if rslt_filelist != nil {
+	//	fmt.Println(rslt_filelist)
+	//}
 
-	name = "obserware"
+	var server *http.Server
+	var expt error
 
-	wait.Add(3)
-	go runner.ImportPrimary(&wait, &rslt_primary, &name)
-	go runner.ImportOther(&wait, &rslt_other, &name)
-	go runner.ImportFileList(&wait, &rslt_filelist, &name)
-	wait.Wait()
+	server = &http.Server{Addr: ":8080", Handler: http.DefaultServeMux}
 
-	if rslt_primary != nil {
-		fmt.Println(rslt_primary)
+	http.HandleFunc("/rawhide/changelog/", routes.RetrieveChangelog)
+
+	expt = server.ListenAndServe()
+	if expt != nil {
+		slog.Log(nil, slog.LevelError, fmt.Sprintf("Error occurred. %s.", expt.Error()))
 	}
 
-	if rslt_other != nil {
-		fmt.Println(rslt_other)
-	}
-
-	if rslt_filelist != nil {
-		fmt.Println(rslt_filelist)
-	}
 }
