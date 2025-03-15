dd32e9f8e5a6c6957baf61cc077b9f2b243422c0
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2025-03-15 05:06:18 +0000

    Optimize the decoding process with tokenizing
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- metasource/runner/file.go
+++ metasource/runner/file.go
@@ -4,37 +4,58 @@ import (
 	"bufio"
 	"encoding/xml"
 	"fmt"
+	"io"
 	"log/slog"
-	"metasource/metasource/models"
+	"metasource/metasource/models/sxml"
 	"os"
 	"sync"
 )
 
-func ImportFileList(wait *sync.WaitGroup, rslt_filelist **models.FileList) {
+func ImportFileList(wait *sync.WaitGroup, rslt_filelist **sxml.UnitFileList, name *string) {
 	defer wait.Done()
 
-	var rslt models.FileList
 	var expt error
 	var file *os.File
 	var data *bufio.Reader
 	var deco *xml.Decoder
+	var chip xml.Token
 
 	file, expt = os.Open("/home/fedohide-origin/projects/metasource/rawhide/4182e96bacb8bb0ccdcc9d446977416a2a18b49a4aed13d6c550be45a1bf061e-filelists.xml")
 	if expt != nil {
 		slog.Log(nil, slog.LevelError, fmt.Sprintf("File could not be loaded. %s.", expt.Error()))
+		return
 	}
+	defer file.Close()
 
 	data = bufio.NewReader(file)
 	deco = xml.NewDecoder(data)
-	expt = deco.Decode(&rslt)
-	if expt != nil {
-		slog.Log(nil, slog.LevelError, fmt.Sprintf("File could not be parsed. %s.", expt.Error()))
-	}
 
-	expt = file.Close()
-	if expt != nil {
-		slog.Log(nil, slog.LevelError, fmt.Sprintf("File could not be closed. %s.", expt.Error()))
-	}
+	for {
+		chip, expt = deco.Token()
+		if expt != nil {
+			if expt != io.EOF {
+				slog.Log(nil, slog.LevelError, fmt.Sprintf("File could not be parsed. %s.", expt.Error()))
+				return
+			}
+			break
+		}
 
-	*rslt_filelist = &rslt
+		switch se := chip.(type) {
+		case xml.StartElement:
+			if se.Name.Local == "package" {
+				var item sxml.UnitFileList
+				expt = deco.DecodeElement(&item, &se)
+				if expt != nil {
+					slog.Log(nil, slog.LevelError, fmt.Sprintf("File could not be solved. %s.", expt.Error()))
+					return
+				}
+				if item.Name == *name {
+					if rslt_filelist != nil {
+						*rslt_filelist = &item
+					}
+					return
+				}
+			}
+		}
+	}
 }
--- metasource/runner/prmy.go
+++ metasource/runner/prmy.go
@@ -4,37 +4,58 @@ import (
 	"bufio"
 	"encoding/xml"
 	"fmt"
+	"io"
 	"log/slog"
-	"metasource/metasource/models"
+	"metasource/metasource/models/sxml"
 	"os"
 	"sync"
 )
 
-func ImportPrimary(wait *sync.WaitGroup, rslt_primary **models.Primary) {
+func ImportPrimary(wait *sync.WaitGroup, rslt_primary **sxml.UnitPrimary, name *string) {
 	defer wait.Done()
 
-	var rslt models.Primary
 	var expt error
 	var file *os.File
 	var data *bufio.Reader
 	var deco *xml.Decoder
+	var chip xml.Token
 
 	file, expt = os.Open("/home/fedohide-origin/projects/metasource/rawhide/10beaa5fb8bb9b8710f4608ea9bf84aff2fb68e5efc7e82bf12b421867ad3d8f-primary.xml")
 	if expt != nil {
 		slog.Log(nil, slog.LevelError, fmt.Sprintf("File could not be loaded. %s.", expt.Error()))
+		return
 	}
+	defer file.Close()
 
 	data = bufio.NewReader(file)
 	deco = xml.NewDecoder(data)
-	expt = deco.Decode(&rslt)
-	if expt != nil {
-		slog.Log(nil, slog.LevelError, fmt.Sprintf("File could not be parsed. %s.", expt.Error()))
-	}
 
-	expt = file.Close()
-	if expt != nil {
-		slog.Log(nil, slog.LevelError, fmt.Sprintf("File could not be closed. %s.", expt.Error()))
-	}
+	for {
+		chip, expt = deco.Token()
+		if expt != nil {
+			if expt != io.EOF {
+				slog.Log(nil, slog.LevelError, fmt.Sprintf("File could not be parsed. %s.", expt.Error()))
+				return
+			}
+			break
+		}
 
-	*rslt_primary = &rslt
+		switch se := chip.(type) {
+		case xml.StartElement:
+			if se.Name.Local == "package" {
+				var item sxml.UnitPrimary
+				expt = deco.DecodeElement(&item, &se)
+				if expt != nil {
+					slog.Log(nil, slog.LevelError, fmt.Sprintf("File could not be solved. %s.", expt.Error()))
+					return
+				}
+				if item.Name == *name {
+					if rslt_primary != nil {
+						*rslt_primary = &item
+					}
+					return
+				}
+			}
+		}
+	}
 }
