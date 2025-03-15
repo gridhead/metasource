26ec5f782689c559cb60655b9a68a98acc7134ce
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2025-03-15 05:07:50 +0000

    Simplify the entrypoint codebase for debugging
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- main.go
+++ main.go
@@ -2,47 +2,35 @@ package main
 
 import (
 	"fmt"
-	"metasource/metasource/models"
+	"metasource/metasource/models/sxml"
 	"metasource/metasource/runner"
 	"sync"
 )
 
 func main() {
 	var name string
-	var rslt_primary *models.Primary
-	var rslt_other *models.Other
-	var rslt_filelist *models.FileList
+	var rslt_primary *sxml.UnitPrimary
+	var rslt_other *sxml.UnitOther
+	var rslt_filelist *sxml.UnitFileList
 	var wait sync.WaitGroup
 
+	name = "obserware"
+
 	wait.Add(3)
-	go runner.ImportPrimary(&wait, &rslt_primary)
-	go runner.ImportOther(&wait, &rslt_other)
-	go runner.ImportFileList(&wait, &rslt_filelist)
+	go runner.ImportPrimary(&wait, &rslt_primary, &name)
+	go runner.ImportOther(&wait, &rslt_other, &name)
+	go runner.ImportFileList(&wait, &rslt_filelist, &name)
 	wait.Wait()
 
-	name = "nano"
-
 	if rslt_primary != nil {
-		for _, unit := range rslt_primary.List {
-			if unit.Name == name {
-				fmt.Println(unit)
-			}
-		}
+		fmt.Println(rslt_primary)
 	}
 
 	if rslt_other != nil {
-		for _, unit := range rslt_other.List {
-			if unit.Name == name {
-				fmt.Println(unit)
-			}
-		}
+		fmt.Println(rslt_other)
 	}
 
 	if rslt_filelist != nil {
-		for _, unit := range rslt_filelist.List {
-			if unit.Name == name {
-				fmt.Println(unit)
-			}
-		}
+		fmt.Println(rslt_filelist)
 	}
 }
