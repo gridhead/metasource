340bf3a1dbe3f25af802fdf0d062a605a2132fd6
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2025-03-21 07:25:59 +0000

    Parse XML file once and parallelize SQLite writes
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- metasource/reader/plus.go
+++ metasource/reader/plus.go
@@ -1,24 +1,30 @@
 package reader
 
 /*
-   #cgo pkg-config: createrepo_c
-   #include <stdlib.h>
-   #include "createrepo_c/xml_parser.h"
-   #include <createrepo_c/sqlite.h>
-   #include <createrepo_c/package.h>
-   #include <createrepo_c/createrepo_c.h>
+#cgo pkg-config: createrepo_c
+#include "createrepo_c/xml_parser.h"
+#include "createrepo_c/package.h"
 */
 import "C"
 
 import (
+	"errors"
+	"fmt"
+	"log/slog"
 	"sync"
 	"unsafe"
 )
 
 func MakeDatabase() (bool, error) {
+	var gexp *C.GError
+	var expt error
+	var iter *C.cr_PkgIterator
 	var wait sync.WaitGroup
 	var prmypath, filepath, othrpath string
 	var prmyconv, fileconv, othrconv *C.char
+	var prmypack, filepack, othrpack chan *C.cr_Package
+	var prmyrslt, filerslt, othrrslt chan bool
+	var pack *C.cr_Package
 
 	prmypath = "/home/fedohide-origin/projects/metasource/rawhide/10beaa5fb8bb9b8710f4608ea9bf84aff2fb68e5efc7e82bf12b421867ad3d8f-primary.xml"
 	filepath = "/home/fedohide-origin/projects/metasource/rawhide/4182e96bacb8bb0ccdcc9d446977416a2a18b49a4aed13d6c550be45a1bf061e-filelists.xml"
@@ -30,11 +36,54 @@ func MakeDatabase() (bool, error) {
 	defer C.free(unsafe.Pointer(fileconv))
 	defer C.free(unsafe.Pointer(othrconv))
 
+	prmypack = make(chan *C.cr_Package, 10)
+	filepack = make(chan *C.cr_Package, 10)
+	othrpack = make(chan *C.cr_Package, 10)
+	prmyrslt = make(chan bool, 10)
+	filerslt = make(chan bool, 10)
+	othrrslt = make(chan bool, 10)
+
 	wait.Add(3)
-	go PopulatePrmy(&wait, &prmyconv, &fileconv, &othrconv)
-	go PopulateFile(&wait, &prmyconv, &fileconv, &othrconv)
-	go PopulateOthr(&wait, &prmyconv, &fileconv, &othrconv)
+	go PopulatePrmy(&wait, prmypack, prmyrslt)
+	go PopulateFile(&wait, filepack, filerslt)
+	go PopulateOthr(&wait, othrpack, othrrslt)
+
+	iter = C.cr_PkgIterator_new(prmyconv, fileconv, othrconv, nil, nil, nil, nil, &gexp)
+	if iter == nil {
+		expt = errors.New(fmt.Sprintf("%s", C.GoString(gexp.message)))
+		slog.Log(nil, slog.LevelError, fmt.Sprintf("%s", expt.Error()))
+		return false, expt
+	}
+	defer C.cr_PkgIterator_free(iter, &gexp)
+
+	for C.cr_PkgIterator_is_finished(iter) == 0 {
+		pack = C.cr_PkgIterator_parse_next(iter, &gexp)
+		if pack == nil {
+			break
+		}
+		prmypack <- pack
+		filepack <- pack
+		othrpack <- pack
+
+		prmydone, _ := <-prmyrslt
+		filedone, _ := <-filerslt
+		othrdone, _ := <-othrrslt
+
+		if prmydone && filedone && othrdone {
+			head := fmt.Sprintf("%s %s:%s-%s.%s", C.GoString(pack.name), C.GoString(pack.epoch), C.GoString(pack.version), C.GoString(pack.release), C.GoString(pack.arch))
+			slog.Log(nil, slog.LevelInfo, fmt.Sprintf("%s added.", head))
+		}
+	}
+	close(prmypack)
+	close(filepack)
+	close(othrpack)
+	close(prmyrslt)
+	close(filerslt)
+	close(othrrslt)
+
 	wait.Wait()
 
+	defer C.free(unsafe.Pointer(pack))
+	defer C.free(unsafe.Pointer(gexp))
 	return true, nil
 }
--- metasource/reader/prmy.go
+++ metasource/reader/prmy.go
@@ -2,11 +2,8 @@ package reader
 
 /*
 #cgo pkg-config: createrepo_c
-#include <stdlib.h>
-#include "createrepo_c/xml_parser.h"
-#include <createrepo_c/sqlite.h>
-#include <createrepo_c/package.h>
-#include <createrepo_c/createrepo_c.h>
+#include "createrepo_c/sqlite.h"
+#include "createrepo_c/package.h"
 */
 import "C"
 import (
@@ -17,7 +14,7 @@ import (
 	"unsafe"
 )
 
-func PopulatePrmy(wait *sync.WaitGroup, prmyconv **C.char, fileconv **C.char, othrconv **C.char) {
+func PopulatePrmy(wait *sync.WaitGroup, dbpk <-chan *C.cr_Package, done chan<- bool) {
 	defer wait.Done()
 
 	var path string
@@ -27,20 +24,11 @@ func PopulatePrmy(wait *sync.WaitGroup, prmyconv **C.char, fileconv **C.char, ot
 	var rslt int
 	var gexp *C.GError
 	var expt error
-	var iter *C.cr_PkgIterator
-	// var head string
-
-	iter = C.cr_PkgIterator_new(*prmyconv, *fileconv, *othrconv, nil, nil, nil, nil, &gexp)
-	if iter == nil {
-		expt = errors.New(fmt.Sprintf("%s", C.GoString(gexp.message)))
-		slog.Log(nil, slog.LevelError, fmt.Sprintf("Failed to create package iterator. %s", expt.Error()))
-		return
-	}
-	defer C.cr_PkgIterator_free(iter, &gexp)
 
 	path = "akashdeeep-primary.sqlite"
 	conv = C.CString(path)
 	defer C.free(unsafe.Pointer(conv))
+
 	base = C.cr_db_open(conv, C.CR_DB_PRIMARY, &gexp)
 	defer C.cr_db_close(base, &gexp)
 	if gexp != nil {
@@ -49,20 +37,13 @@ func PopulatePrmy(wait *sync.WaitGroup, prmyconv **C.char, fileconv **C.char, ot
 		return
 	}
 
-	for C.cr_PkgIterator_is_finished(iter) == 0 {
-		pack = C.cr_PkgIterator_parse_next(iter, &gexp)
-		if pack == nil {
-			break
-		}
-		// head = fmt.Sprintf("%s %s-%s.%s", C.GoString(pack.name), C.GoString(pack.version), C.GoString(pack.epoch), C.GoString(pack.release))
-		// slog.Log(nil, slog.LevelInfo, fmt.Sprintf("%s added.", head))
+	for pack = range dbpk {
 		rslt = int(C.cr_db_add_pkg(base, pack, &gexp))
 		if rslt != 0 {
 			expt = errors.New(fmt.Sprintf("%s", C.GoString(gexp.message)))
-			slog.Log(nil, slog.LevelWarn, fmt.Sprintf("Failed to add package. %s", expt.Error()))
+			slog.Log(nil, slog.LevelWarn, fmt.Sprintf("%s", expt.Error()))
+			done <- false
 		}
+		done <- true
 	}
-
-	defer C.free(unsafe.Pointer(pack))
-	defer C.free(unsafe.Pointer(gexp))
 }
