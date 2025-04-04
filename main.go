40036d293ac6bf7a057cdff0fbf29444d23afe3f
Akashdeep Dhar <akashdeep.dhar@gmail.com>
2025-04-04 07:47:41 +0000

    Move from default `net/http` mux to `go-chi/chi/v5` mux
    Signed-off-by: Akashdeep Dhar <akashdeep.dhar@gmail.com>

--- main.go
+++ main.go
@@ -3,56 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"log/slog"
 	"metasource/metasource/config"
 	"metasource/metasource/driver"
+	"metasource/metasource/routes"
+	"net/http"
 	"os"
 )
 
 func main() {
-	//var name string
-	//var rslt_primary *sxml.UnitPrimary
-	//var rslt_other *sxml.UnitOther
-	//var rslt_filelist *sxml.UnitFileList
-	//var wait sync.WaitGroup
-	//
-	//name = "obserware"
-	//
-	//wait.Add(3)
-	//go runner.ImportPrimary(&wait, &rslt_primary, &name)
-	//go runner.ImportOther(&wait, &rslt_other, &name)
-	//go runner.ImportFileList(&wait, &rslt_filelist, &name)
-	//wait.Wait()
-	//
-	//if rslt_primary != nil {
-	//	fmt.Println(rslt_primary)
-	//}
-	//
-	//if rslt_other != nil {
-	//	fmt.Println(rslt_other)
-	//}
-	//
-	//if rslt_filelist != nil {
-	//	fmt.Println(rslt_filelist)
-	//}
-
-	// ACTUAL CODE
-	//var server *http.Server
-	//var expt error
-	//
-	//
-	//server = &http.Server{Addr: ":8080", Handler: http.DefaultServeMux}
-	//
-	//http.HandleFunc("/rawhide/changelog/", routes.RetrieveOther)
-	//http.HandleFunc("/rawhide/pkg/", routes.RetrievePrimary)
-	//http.HandleFunc("/rawhide/files/", routes.RetrieveFileList)
-	//http.HandleFunc("/rawhide/", routes.RetrievePlus)
-	//
-	//expt = server.ListenAndServe()
-	//if expt != nil {
-	//	slog.Log(nil, slog.LevelError, fmt.Sprintf("Error occurred. %s.", expt.Error()))
-	//}
-
 	var expt error
 	var lglvtext, location *string
 	var database, dispense *flag.FlagSet
@@ -60,6 +21,8 @@ func main() {
 	lglvtext = flag.String("loglevel", "info", "Set the application loglevel")
 	location = flag.String("location", config.DBFOLDER, "Set the database location")
 	flag.Parse()
+
+	config.DBFOLDER = *location
 
 	database = flag.NewFlagSet("database", flag.ExitOnError)
 	dispense = flag.NewFlagSet("dispense", flag.ExitOnError)
@@ -90,8 +53,26 @@ func main() {
 			slog.Log(nil, slog.LevelError, fmt.Sprintf("%s", expt.Error()))
 			os.Exit(1)
 		}
-		slog.Log(nil, slog.LevelError, "Dispense is not implemented yet")
-		os.Exit(1)
+
+		var router *chi.Mux
+		var server *http.Server
+
+		router = chi.NewRouter()
+		router.Use(middleware.Logger)
+		router.Use(middleware.Recoverer)
+
+		router.Get("/{vers}/changelog/{name}", routes.RetrieveOther)
+		router.Get("/{vers}/pkg/{name}", routes.RetrievePrimary)
+		router.Get("/{vers}/files/{name}", routes.RetrieveFileList)
+		router.Get("/{vers}/srcpkg/{name}", routes.RetrieveSrce)
+		router.Get("/{vers}/{rela}/{name}", routes.RetrieveRelation)
+
+		server = &http.Server{Addr: ":8080", Handler: router}
+
+		expt = server.ListenAndServe()
+		if expt != nil {
+			slog.Log(nil, slog.LevelError, fmt.Sprintf("Error occurred. %s.", expt.Error()))
+		}
 	default:
 		slog.Log(nil, slog.LevelError, "Invalid subcommand")
 		slog.Log(nil, slog.LevelInfo, "Expected either 'database' or 'dispense' subcommand")
